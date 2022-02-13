02519588e4795430c7f43dbab14d8b7156ec46a8
Jeevan Cyriac <jeevantom@hotmail.com>
2022-02-13 21:48:20 +0000

    tf fmt

--- terraform/budgets.tf
+++ terraform/budgets.tf
@@ -1,9 +1,9 @@
 resource "aws_budgets_budget" "monthly" {
-  name              = "monthly-1-dollar"
-  budget_type       = "COST"
-  limit_amount      = "1"
-  limit_unit        = "USD"
-  time_unit         = "MONTHLY"
+  name         = "monthly-1-dollar"
+  budget_type  = "COST"
+  limit_amount = "1"
+  limit_unit   = "USD"
+  time_unit    = "MONTHLY"
 
   notification {
     comparison_operator        = "GREATER_THAN"
